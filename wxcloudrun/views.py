94ec197d3489d4da12dff8553ca5f736a341e912
liuyiquan <liu1quan@qq.com>
2024-07-19 10:25:48 +0800

    try

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -71,7 +71,7 @@ def get_count():
 @app.route('/api/user_exists', methods=['GET'])
 def user_exists():
     headers = dict(request.headers)
-    openid = '1111' # headers['X-Wx-0penid']
+    openid = headers.get('X-Wx-0penid','openidnotfound')
     user = query_user_by_openid(openid)
     if user is None:
         return make_succ_response({'header':headers, 'exists':0})
